853f23ddfb903c4237bf7dcae8b87bcf01a75567
unknown <leonardo.marcarini@yahoo.com.br>
2021-05-15 23:24:55 -0300

    Corrigido sintaxe e acrescentado funcao de inserir

--- insere.php
+++ insere.php
@@ -8,9 +8,11 @@ try{
     throw new \PDOException($e->getMessage(),(int)$e->getCode());
 }
 
-$searchid = '1';
-$query=$pdo->prepare('SELECT Titulo FROM Filmes WHERE id = ?');
-$query->execute([$searchid]);
+$titulo="Tenet";
+$ano="2020";
+$imdb="6723592";
+$query=$pdo->prepare('INSERT INTO Filmes (Titulo, Ano, imdb) VALUES (?,?,?)');
+$query->execute([$titulo,$ano,$imdb]);
 $movie=$query->fetch();
 
 echo json_encode($movie);
