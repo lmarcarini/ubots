03ee4ab7b939bc38c8c32d5b6c3cc03ac8336459
unknown <leonardo.marcarini@yahoo.com.br>
2021-05-18 11:02:20 -0300

    Alteracoes nas queries

--- Filmes.php
+++ Filmes.php
@@ -9,8 +9,19 @@ class Filmes{
     }
     
     public function getFilmes(){
-        $sql='SELECT Titulo FROM Filmes';
+        $user_id=isset($_GET['id'])?$_GET['id']:null;
+        $offset=isset($_GET['offset'])?$_GET['offset']*4:0;
+        if($user_id!=null){
+            $sql='SELECT filmes.*, avaliacoes.avaliacao FROM filmes LEFT JOIN avaliacoes ON (filmes.id=avaliacoes.filmeId AND avaliacoes.userId= ? ) LIMIT ?, 4';
+                $query=$this->conection->prepare($sql);
+                $query->bindValue(1, $user_id,PDO::PARAM_INT);
+                $query->bindValue(2, $offset,PDO::PARAM_INT);
+                $query->execute();
+                return $query;
+        }
+        $sql='SELECT id, Titulo, Ano, imdb FROM filmes LIMIT ?, 4';
         $query=$this->conection->prepare($sql);
+        $query->bindValue(1, $offset,PDO::PARAM_INT);
         $query->execute();
         return $query;
     }
@@ -19,7 +30,7 @@ class Filmes{
         $titulo=$_GET['titulo'];
         $ano=$_GET["ano"];
         $imdb=$_GET["imdb"];
-        $sql='INSERT INTO Filmes (Titulo, Ano, imdb) VALUES (?,?,?)';
+        $sql='INSERT INTO filmes (Titulo, Ano, imdb) VALUES (?,?,?)';
         $query=$this->conection->prepare($sql);
         return $query->execute([$titulo,$ano,$imdb]);
     }
@@ -29,14 +40,14 @@ class Filmes{
         $titulo=$_GET['titulo'];
         $ano=$_GET["ano"];
         $imdb=$_GET["imdb"];
-        $sql='UPDATE Filmes SET Titulo= ? , Ano= ? , imdb= ? WHERE id=?';
+        $sql='UPDATE filmes SET Titulo= ? , Ano= ? , imdb= ? WHERE id=?';
         $query=$this->conection->prepare($sql);
         return $query->execute([$titulo,$ano,$imdb,$id]);
     }
 
     public function deletaFilme(){
         $id=$_GET['id'];
-        $sql='DELETE FROM Filmes WHERE id = ?';
+        $sql='DELETE FROM filmes WHERE id = ?';
         $query=$this->conection->prepare($sql);
         return $query->execute([$id]);
     }
@@ -45,24 +56,24 @@ class Filmes{
         $filme_id=$_GET['filmeId'];
         $user_id=$_GET['userId'];
         $score=$_GET['score'];
-        $sql='SELECT id FROM Avaliacoes WHERE userId=? AND filmeId= ?';
+        $sql='SELECT id FROM avaliacoes WHERE userId=? AND filmeId= ?';
         $query=$this->conection->prepare($sql);
         $query->execute([$user_id,$filme_id]);
         $result=$query->fetch();
         if(!$result) {
-            $sql='INSERT INTO Avaliacoes (userId, filmeId, avaliacao) VALUES (?,?,?)';
+            $sql='INSERT INTO avaliacoes (userId, filmeId, avaliacao) VALUES (?,?,?)';
             $query=$this->conection->prepare($sql);
             return $query->execute([$user_id,$filme_id,$score]);
         }
         $id=$result['id'];
-        $sql='UPDATE Avaliacoes SET userId= ? , filmeId = ? , avaliacao= ? WHERE id=?';
+        $sql='UPDATE avaliacoes SET userId= ? , filmeId = ? , avaliacao= ? WHERE id=?';
         $query=$this->conection->prepare($sql);
         return $query->execute([$user_id,$filme_id,$score,$id]);
     }
 
     public function sugereFilme(){
-        $user_id=1;//$_GET['userId'];
-        $sql='SELECT * FROM Filmes WHERE id NOT IN (SELECT filmeId FROM Avaliacoes WHERE userId = ?)';
+        $user_id=isset($_GET['userId'])?$_GET['userId']:1;
+        $sql='SELECT * FROM filmes WHERE id NOT IN (SELECT filmeId FROM avaliacoes WHERE userId = ?)';
         $query=$this->conection->prepare($sql);
         $query->execute([$user_id]);
         return $query;
